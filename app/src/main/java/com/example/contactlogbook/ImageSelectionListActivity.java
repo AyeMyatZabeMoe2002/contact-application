f5de643e1594e923158467c516deb57bc3790267
AyeMyatZabeMoe <ayemyatzabemoe12dev@gmail.com>
2024-11-14 08:53:51 +0630

    first commit

---
+++ app/src/main/java/com/example/contactlogbook/ImageSelectionListActivity.java
@@ -0,0 +1,13 @@
+package com.example.contactlogbook;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ImageSelectionListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_image_selection_list);
+
+    }//end of onCreate
+}//end of class
---
+++ app/src/main/java/com/example/contactlogbook/PersonEntryActivity.java
@@ -0,0 +1,11 @@
+package com.example.contactlogbook;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+public class PersonEntryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_person_entry);
+    }//end of onCreate
+}//end of class
